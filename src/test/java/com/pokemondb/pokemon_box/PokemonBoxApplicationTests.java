613fbce52710be97b834c61252e671e1e897f2b3
joshhkim02 <joshhkim02@gmail.com>
2025-06-09 18:03:10 -0700

    feat: initial commit

---
+++ src/test/java/com/pokemondb/pokemon_box/PokemonBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemondb.pokemon_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
