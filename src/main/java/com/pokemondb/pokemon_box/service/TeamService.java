fb21580434c915d8ad5a470ae59cbf17371b418a
joshhkim02 <joshhkim02@gmail.com>
2025-06-10 17:37:13 -0700

    feat: create and read for teamData

--- src/main/java/com/pokemondb/pokemon_box/service/TeamService.java
+++ src/main/java/com/pokemondb/pokemon_box/service/TeamService.java
@@ -8,6 +8,7 @@ import com.pokemondb.pokemon_box.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,23 +25,34 @@ public class TeamService {
     }
 
     // Read specific team
-    public Optional<Team> getTeamById(int id) {
+    public Optional<Team> getTeamById(Integer id) {
         return teamRepository.findById(id);
     }
 
     // Update team
-    public Team updateTeamName(int teamId, String newName) {
+    public Team updateTeamName(Integer teamId, String newName) {
         Team team = teamRepository.findById(teamId).orElseThrow(() -> new RuntimeException("Team not found"));
-
         team.setTeam_name(newName);
-
         return teamRepository.save(team);
     }
 
     // Delete team
-    public void deleteTeam(int id){
+    public void deleteTeam(Integer id){
         Team team = teamRepository.findById(id).orElseThrow(() -> new RuntimeException("Team not found"));
         teamRepository.delete(team);
     }
-    
+
+    // Create Pokemon for team
+    public TeamData addPokemonToTeam(Integer teamId, Pokemon pokemon, Integer position) {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new RuntimeException("Team not found"));
+
+        TeamData teamData = new TeamData(team, pokemon, position);
+        return teamDataRepository.save(teamData);
+    }
+
+    // Read Pokemon from team
+    public List<TeamData> getPokemonFromTeam(List<Integer> teamId) {
+        return teamDataRepository.findAllById(teamId);
+    }
 }
